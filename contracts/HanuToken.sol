9431ad8a165b69d60a117aefb7f23317556d4e64
hgarg242 <hgarg242@gmail.com>
2021-12-28 20:56:44 +0530

    Transfer from

--- contracts/HanuToken.sol
+++ contracts/HanuToken.sol
@@ -9,9 +9,18 @@ contract HanuToken {
 
     uint256 public totalSupply;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint256 _value);
 
+    event Approval(
+        address indexed _owner, 
+        address indexed _spender, 
+        uint256 _value);
+    
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _intiialSupply) {
         //only internal functions should be called in constructor
@@ -32,6 +41,34 @@ contract HanuToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    //approve func
+    //set allowance
+    //trigger Approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    //transferFrom func
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        //Require _from has enough tokens
+        //Require allowance is big enough
+        //change the balance
+        //update allowance
+        // Transfer event
+        // return a boolean
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/HanuToken.js
+++ test/HanuToken.js
@@ -59,7 +59,62 @@ contract("HanuToken", function(accounts) {
         });
     });
 
+    it("approves token for delegated transfer", function() {
+        return HanuToken.deployed().then(function(tok) {
+          token = tok;
+          return token.approve.call(accounts[1],100);
+        }).then(function(res) {
+            assert.equal(res, true, 'Call should returns true')
+            return token.approve(accounts[1], 100, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return token.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), (100), 'Authroized amount');
+        });
+    });
+
+    it("handling deledated token transfers", function() {
+            return HanuToken.deployed().then(function(tok) {
+            token = tok;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+
+            return token.transfer(fromAccount, 100, {from: accounts[0]})
+        }).then(function(receipt) {
+            return token.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(function(receipt){
+            return token.transferFrom(fromAccount, toAccount, 500, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'CanNot transfer Value larger than balance');
+            return token.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'CanNot transfer Value larger than allowed tokens');
+            return token.transferFrom.call(fromAccount, toAccount, 5, {from: spendingAccount});
+        }).then(function(res){
+            assert.equal(res, true, 'Call should returns true');
+            return token.transferFrom(fromAccount, toAccount, 5, {from: spendingAccount});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 5, 'logs the transfer amount');
+            return token.balanceOf(toAccount);
+        }).then(function(bal){
+            assert.equal(bal.toNumber(), (5), 'Amount not transfered correctly to the receving acount');
+            return token.balanceOf(fromAccount);
+        }).then(function(bal){
+            assert.equal(bal.toNumber(), (95), 'Amount not transfered correctly from the sender acount');
+            return token.allowance(fromAccount, spendingAccount);
+        }).then(function(bal){
+            assert.equal(bal.toNumber(), (5), 'Allowance not deducted correctly for the spender acount');
+        });
+    });
 
 })
-
-
