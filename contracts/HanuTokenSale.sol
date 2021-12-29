b10ef5a92adc49dcdfcdb43b784ff58d3292b494
hgarg242 <hgarg242@gmail.com>
2021-12-29 11:21:23 +0530

    Token sale contract complete

--- contracts/HanuTokenSale.sol
+++ contracts/HanuTokenSale.sol
@@ -1,17 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.9.0;
 import "./HanuToken.sol";
+import "./utils/math/SafeMath.sol";
 
 contract HanuTokenSale {
     address admin;
     HanuToken public tokenContract;
 
+    using SafeMath for uint256;
+
     //in Wei
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(
+        address indexed _buyer, 
+        uint256 _amount);
+    
 
     constructor(HanuToken _tokenContract, uint256 _tokenPrice) {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    //multiply
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == _numberOfTokens.mul(tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        //requrie that a transfer is succesfull
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/HanuTokenSale.js
+++ test/HanuTokenSale.js
@@ -1,10 +1,13 @@
 const HanuTokenSale = artifacts.require("HanuTokenSale");
+const HanuToken = artifacts.require("HanuToken");
 
 contract("HanuTokenSale", function(accounts) {
 
-
     //in wei
     tokenPrice=1000000000000000;
+    admin = accounts[0];
+    buyer = accounts[1];
+    tokensAvailable = 7500;
 
     it("intializes the contract with correct values upon deployment", function() {
         return HanuTokenSale.deployed().then(function(tok) {
@@ -20,5 +23,39 @@ contract("HanuTokenSale", function(accounts) {
             assert.equal(price, tokenPrice, 'token price is correct');
         });
     });
+
+    it("facilititates token buying", function() {
+        return HanuToken.deployed().then(function(tokenHanu){
+            token = tokenHanu;
+            return HanuTokenSale.deployed();
+        }).then(function(tok) {
+          tokenSale = tok;
+          return token.transfer(tokenSale.address, tokensAvailable, {from: admin});
+        }).then(function(receipt) {
+          numberOfTokens = 100;
+          amountInWei = numberOfTokens*tokenPrice;
+          return tokenSale.buyTokens(numberOfTokens, {from: buyer, value: amountInWei});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSale.tokensSold();
+        }).then(function(amount){
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            return tokenSale.buyTokens(numberOfTokens, {from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.toString().indexOf('revert')>=0, 'msg.value in wei must equal to actual value of no. of tokens');
+            return tokenSale.buyTokens(8000, {from: buyer, value: 8000*tokenPrice});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'number of tokens purchased can not be more than avaible in conrtact');
+            return tokenSale.buyTokens(80, {from: buyer, value: 80*tokenPrice});
+        }).then(function(receipt){
+            return token.balanceOf(tokenSale.address);
+        }).then(function(bal){
+            assert.equal(bal.toNumber(), tokensAvailable-numberOfTokens-80, 'Number tokens available decreases');
+            
+        });
+    }); 
 
 });
